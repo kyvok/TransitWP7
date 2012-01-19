13365ef55b0e13be35dd2d93ae17249358e9bab3
Christopher Scrosati <christopher.scrosati@gmail.com>
2012-01-18 20:42:27 -0800

    Creating Library with Bing Apis

---
+++ BingApisLib/BingMapsExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿//TODO: copyright info
+
+namespace BingApisLib
+{
+    using System.Device.Location;
+    using Microsoft.Phone.Controls.Maps;
+
+    public static class BingMapsExtensionMethods
+    {
+        public static BingMapsRestApi.BoundingBox AsBingMapsBoundingBox(this LocationRect locRect)
+        {
+            return new BingMapsRestApi.BoundingBox(locRect.South, locRect.West, locRect.North, locRect.East);
+        }
+
+        public static BingMapsRestApi.Point AsBingMapsPoint(this GeoCoordinate geoCoord)
+        {
+            return new BingMapsRestApi.Point(geoCoord.Latitude, geoCoord.Longitude);
+        }
+    }
+}
---
+++ BingApisLib/BingMapsRestApi/BingMapsQueryAsyncCallback.cs
@@ -0,0 +1,44 @@
+﻿//TODO: copyright info
+
+namespace BingApisLib.BingMapsRestApi
+{
+    using System;
+    using System.Net;
+
+    internal class BingMapsQueryAsyncCallback
+    {
+        private Action<BingMapsQueryResult> callback;
+        private object userState;
+
+        public BingMapsQueryAsyncCallback(Action<BingMapsQueryResult> callback, object userState)
+        {
+            if (callback == null)
+            {
+                throw new ArgumentNullException("callback");
+            }
+
+            this.callback = callback;
+            this.userState = userState;
+        }
+
+        public HttpWebRequest HttpWebRequest { get; private set; }
+
+        /// <summary>
+        /// Invokes the callback with a result that contains an exception.
+        /// </summary>
+        /// <param name="exception">The exception that occured.</param>
+        public void Notify(Exception exception)
+        {
+            this.callback(new BingMapsQueryResult(exception, userState));
+        }
+
+        /// <summary>
+        /// Invokes the callback with a result that contains the response.
+        /// </summary>
+        /// <param name="response">The Bing Maps response object.</param>
+        public void Notify(Response response)
+        {
+            this.callback(new BingMapsQueryResult(response, userState));
+        }
+    }
+}
