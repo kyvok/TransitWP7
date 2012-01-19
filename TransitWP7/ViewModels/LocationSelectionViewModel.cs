f62b06754d6f38e95d5d732b67bc5fc4c46052df
Christopher Scrosati <christopher.scrosati@gmail.com>
2012-01-19 01:11:55 -0800

    adding missing viewmodel files

---
+++ TransitWP7/ViewModels/LocationSelectionViewModel.cs
@@ -0,0 +1,22 @@
+﻿
+namespace TransitWP7.ViewModels
+{
+    using System;
+    using System.Net;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Documents;
+    using System.Windows.Ink;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Animation;
+    using System.Windows.Shapes;
+
+    public class LocationSelectionViewModel : ViewModelBase
+    {
+        public LocationSelectionViewModel()
+        {
+            
+        }
+    }
+}
---
+++ TransitWP7/ViewModels/ViewModelBase.cs
@@ -0,0 +1,18 @@
+﻿
+namespace TransitWP7.ViewModels
+{
+    using System.ComponentModel;
+
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
