0fd928c207c53e127c9b3cf3d56a5ab52e00c30b
Christopher Scrosati <christopher.scrosati@gmail.com>
2012-03-18 11:08:56 -0700

    Transitive rebranding

--- TransitWP7/Properties/AssemblyInfo.cs
+++ TransitWP7/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TransitWP7")]
+[assembly: AssemblyTitle("Transitive")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TransitWP7")]
+[assembly: AssemblyProduct("Transitive")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
