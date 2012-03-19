5239b889dd8bbe3999fdb949f9b80f4a39b43ca5
Christopher Scrosati <christopher.scrosati@gmail.com>
2012-03-18 23:00:23 -0700

    Fixes #31 No time in transit picker when walking only

---
+++ TransitWP7/Converters/TransitTypeToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace TransitWP7.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    public class TransitTypeToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((TransitDescription.DirectionType)value == TransitDescription.DirectionType.Transit)
+            {
+                return Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
