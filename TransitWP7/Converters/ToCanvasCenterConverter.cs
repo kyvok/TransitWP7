f2e05971c88741333e43aee8d123a052124e4b88
Christopher Scrosati <christopher.scrosati@gmail.com>
2012-05-01 00:43:41 -0700

    adding mising files to previous commit

---
+++ TransitWP7/Converters/ToCanvasCenterConverter.cs
@@ -0,0 +1,20 @@
+﻿namespace TransitWP7.Converters
+{
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class ToCanvasCenterConverter : IValueConverter
+    {
+        public object Convert(object value, System.Type targetType, object parameter, CultureInfo culture)
+        {
+            var objectSize = System.Convert.ToDouble(value, CultureInfo.InvariantCulture);
+            var canvasSize = System.Convert.ToDouble(parameter, CultureInfo.InvariantCulture);
+            return (canvasSize - objectSize) / 2;
+        }
+
+        public object ConvertBack(object value, System.Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
